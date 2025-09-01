e367d0f6649311bbd6e902c3b6817c2502e7112c
NVadim <palaginvad@gmail.com>
2025-09-01 19:42:34 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'CV'; 
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  assetPrefix: isProd ? `/${repo}/` : '',
   images: {
     unoptimized: true,
   },
-  basePath: '/CV',
-  assetPrefix: '/CV',
 };
 
 module.exports = nextConfig;
